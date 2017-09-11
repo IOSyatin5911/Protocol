1b269b550e317dd7e940d47e7c6290621e8a333a
user11 <user11@user11s-Mac-mini.local>
2017-09-11 11:04:56 -0400

    Initial Commit

---
+++ swiftProtocol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftProtocol
+//
+//  Created by user11 on 9/11/17.
+//  Copyright © 2017 user11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
